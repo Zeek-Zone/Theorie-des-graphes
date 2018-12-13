2a87253c031dce11ff5a8141cc1adb14000510f0
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2018-12-13 12:13:53 +0000

    Update graphe_matrice.h

--- Mini Projet- Impl-mentations des graphes/graphe_matrice.h
+++ Mini Projet- Impl-mentations des graphes/graphe_matrice.h
@@ -1,5 +1,5 @@
-#ifndef H_GRAPHE
-#define H_GRAPHE
+#ifndef H_GRAPHEM
+#define H_GRAPHEM
 /**
 *   fichier header contenant les declarations de toutes mes structures
 *   et fonctions des graphes représenté sous forme de matrice.
@@ -7,15 +7,23 @@
 *   le 15/11/2018
 */
 
-#define MAX 100
+#define MAX 50 // nombre max de sommets : à adapter selon le besoin
 
 /****************************************************************/
-// Définition de la structure matrice d'adjacence:
+// Définition des structures matrices:
 struct matAdj{
           int  n; // nombre de sommets
           int matrice[MAX][MAX];
 };
 typedef  struct matAdj MatriceDAdjacence;
+
+struct matIncid{
+          int numSommet; // nombre ligne
+          int numLiaison; // nombre colonne
+          int matrice[MAX][MAX];
+};
+typedef  struct matIncid MatriceDIncidence;
+
 
 MatriceDAdjacence creerMatAdjGO();
 MatriceDAdjacence creerMatAdjGNO();
@@ -23,6 +31,13 @@ void afficheArcs(MatriceDAdjacence m);
 void afficheAretes(MatriceDAdjacence m);
 void afficheMatriceAdjacence(MatriceDAdjacence m);
 
+MatriceDIncidence creerMatIncidGNO();
+void afficheMatriceIncidence(MatriceDIncidence  m);
+void afficheAreteMatriceIncidence(MatriceDIncidence m);
+MatriceDIncidence creerMatIncidGO();
+void afficheMatriceIncidence(MatriceDIncidence m);
+void afficheArcMatriceIncidence(MatriceDIncidence m);
 
-#endif // H_GRAPHE
+
+#endif // H_GRAPHEM
 
