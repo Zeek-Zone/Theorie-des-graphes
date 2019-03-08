9f2cef801e073bb677ac3579db43caadf04fe08f
Hassan Zekkouri <hassanzekkouri@gmail.com>
2019-03-08 21:04:59 +0100

    Add CFC

--- Mini Projet 3 - autre algorithmes/composanteFconnexe.c
+++ Mini Projet 3 - autre algorithmes/composanteFconnexe.c
@@ -2,6 +2,24 @@
 #include "graphe_matrice.h"
 #include "utilitaires.h"
 #include "pile.h"
+#include "composanteFconnexe.h"
+
+int nbrArcMarque = 0;
+
+
+int* fusioner(int *tab1, int n, int *tab2, int m){
+    int taille = n + m;
+    int *res = (int*)malloc(taille*sizeof(int));
+    int i,j;
+    for(i = 0, j = 0; i < n ; i++ , j++){
+        res[j] = tab1[i];
+    }
+
+    for(i = 0; i < m ; i++ , j++){
+        res[j] = tab2[i];
+    }
+return res;
+}
 
 int choisirUnSommet(int *sommetDansCFC, int n){
     int i;
@@ -12,43 +30,146 @@ int choisirUnSommet(int *sommetDansCFC, int n){
 return -1;
 }
 
-void composanteFortementConnexe(MatriceDAdjacence graphe){
-    CFC *cfc = creerCFC();
-    Pile *P = initialiser();
-    int *sommetDansCFC = (int*)malloc(graphe.n, sizeof(int));
-    int *sommetDansP = (int*)malloc(graphe.n, sizeof(int));
-    int nombreSommetDansCFC = 0;
+int trouverArcNonMarqué(MatriceDAdjacence graphe, Pile *P, int *sommetDansCFC, Arc *arcsMarque){
 
-    while(nombreSommetDansCFC < graphe.n){
+    Element *top = P->premier;
 
-        s = choisirUnSommet(sommetDansCFC, graphe.n);
-        Composante *c = compsanteEmpile(s);
-        emiler(P, c);
-        sommetDansP[s] = 1;
-
-        while(P->taille > 0){
-                s = trouverArcNonMarqué(P, sommetDansCFC);
-                if(s != -1){
-                    if(sommetDansP[s] == 0){
-                        Composante *cc = compsanteEmpile(s);
-                        emiler(P, cc);
-                        sommetDansP[s] = 1;
-                    }else{
-                        int i = trouverComposante(CFC, s);
-                        metreAjourLaPile(P, i);
+    // Cherchons un arc sortant de "top" et non encore marqué
+    int s, i, j;
+    for(i = 0; i < top->comp->nbrSommet; i++){
+            // chercher un arc non marqué
+            // et rrtourener son extremite terminal j
+            s = top->comp->sommets[i];
+            for(j = 0 ; j < graphe.n ; j++){
+                if(graphe.matrice[s][j] != 0){
+                    if(arcNestPasMarque(arcsMarque, s, j) == 1){
+                        return j;
                     }
-                }else{
-                    c = deplier(P);
-                    marquerSommet(c, sommetDansCFC);
                 }
-        }
+            }
     }
+
+return -1;
 }
+
+int trouverComposante(Pile *P, int s){
+    int i = 0;
+    Element *courant = P->premier;
+    while(courant != NULL){
+            int j ;
+            for(j = 0; j < courant->comp->nbrSommet ; j++){
+                if(courant->comp->sommets[j] == s)
+                    return i;
+            }
+            i++;
+    }
+return -1;
+}
+
+void mettreAjourLaPile(Pile *P, int indice){
+    int i = 1, taille = 0;
+    Element *pred = P->premier,
+            *courant = pred->suivant;
+    int *tab = fusioner(pred->comp->sommets, pred->comp->nbrSommet, courant->comp->sommets, courant->comp->nbrSommet);
+    taille += pred->comp->nbrSommet + courant->comp->nbrSommet;
+
+    while(i != indice && courant->suivant != NULL){
+            pred = courant;
+            courant = pred->suivant;
+            i++;
+            tab = fusioner(tab, taille, courant->comp->sommets, courant->comp->nbrSommet);
+            taille += courant->comp->nbrSommet;
+    }
+    // dépiler "indice" fois
+    for(i = 0; i <= indice ; i++){
+        free( depiler(P) );
+    }
+    // emplier l'union (les composante fusionnée
+    Composante *c = creerComposante();
+    c->nbrSommet = taille;
+    c->sommets = tab;
+    empiler(P, c);
+return;
+}
+
+void marquerSommet(Composante c, int *sommetDansCFC){
+    int i;
+    for(i = 0; i < c->nbrSommet ; i++){
+        sommetDansCFC[ c->sommets[i] ] = 1
+    }
+return;
+}
+
+
+void marquerArc(Arc *arcMarque, int s, int ss){
+    nbrArcMarque++;
+    arcMarque = realloc(arcMarque, nbrArcMarque * sizeof(Arc));
+
+    arcMarque[nbrArcMarque - 1].src = s;
+    arcMarque[nbrArcMarque - 1].dest = ss;
+    arcMarque[nbrArcMarque - 1].poids = 1; // pas intéressant
+}
+
+int arcNestPasMarque(Arc *arcMarque, int s, int ss){
+    int i;
+    for(i = 0; i < nbrArcMarque ; i++){
+        if(arcMarque[i].src == s && arcMarque[i].dest == ss)
+            return 0;
+    }
+return 1;
+}
+
 
 Composante *compsanteEmpile(int s){
         Composante *c = creerComposante();
         c->nbrSommet = 1;
-        c->sommets = (int*)realloc(c->sommets, c->nbrSommet * sizeof(int));
-        c->sommets[c->nbrSommet - 1] = s;
+        c->sommets = (int*)realloc(c->sommets, sizeof(int));
+        c->sommets[0] = s;
 return c;
 }
+
+void composanteFortementConnexe(MatriceDAdjacence graphe){
+
+    CFC *cfc = creerCFC(); // les composantes fortement connexes
+
+    Pile *P = initialiser();
+    int *sommetDansCFC = (int*)malloc(graphe.n, sizeof(int));
+    int *sommetDansP = (int*)malloc(graphe.n, sizeof(int));
+    Arc *arcMarque = (Arc*)malloc(sizeof(Arc));
+    int nombreSommetDansCFC = 0;
+    int s , ss;
+
+    // tant que il exite des sommets non encore regrouper dans une composante
+    while(nombreSommetDansCFC < graphe.n){
+
+        s = choisirUnSommet(sommetDansCFC, graphe.n);
+        Composante *c = compsanteEmpile(s);
+        empiler(P, c);
+        sommetDansP[s] = 1;
+
+        while(P->taille > 0){
+
+                ss = trouverArcNonMarqué(graphe, P, sommetDansCFC, arcMarque);
+                if(s != -1){
+                    if(sommetDansP[s] == 0){
+                        Composante *cc = compsanteEmpile(s);
+                        emiler(P, cc);
+                        sommetDansP[ss] = 1;
+                        marquerArc(arcMarque, s, ss);
+                    }else{
+                        int i = trouverComposante(P, s);
+                        if(i > 0){
+                            mettreAjourLaPile(P, i);
+                        }
+                    }
+                }else{
+                    c = deplier(P);
+                    marquerSommet(c, sommetDansCFC);
+                    nombreSommetDansCFC += c->nbrSommet;
+                    free(c);
+                }
+        }
+    }
+return;
+}
+
--- Mini Projet 3 - autre algorithmes/composanteFconnexe.h
+++ Mini Projet 3 - autre algorithmes/composanteFconnexe.h
@@ -1,1 +1,23 @@
 
+
+
+int* fusioner(int *tab1, int n, int *tab2, int m);
+
+int choisirUnSommet(int *sommetDansCFC, int n);
+
+int trouverArcNonMarqué(MatriceDAdjacence graphe, Pile *P, int *sommetDansCFC, Arc *arcsMarque);
+
+int trouverComposante(Pile *P, int s);
+
+void mettreAjourLaPile(Pile *P, int indice);
+
+void marquerSommet(Composante c, int *sommetDansCFC);
+
+void marquerArc(Arc *arcMarque, int s, int ss);
+
+int arcNestPasMarque(Arc *arcMarque, int s, int ss);
+
+Composante *compsanteEmpile(int s);
+
+void composanteFortementConnexe(MatriceDAdjacence graphe);
+
