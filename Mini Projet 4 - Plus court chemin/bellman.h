8d1adf11fdc27b81793926f55b5cb16d026686bc
DgrinderHZ <hassanzekkouri@gmail.com>
2020-01-11 03:41:15 +0100

    Add project4

---
+++ Mini Projet 4 - Plus court chemin/bellman.h
@@ -0,0 +1,12 @@
+
+
+int* triTopo(MatriceDAdjacence graphe, int x0);
+
+void Bellman(MatriceDAdjacence graphe, int x0);
+
+
+int sommetSansPred(MatriceDAdjacence graphe, int *sommetTrie);
+
+int sommetSuivantANumerote(MatriceDAdjacence graphe, int *sommetTrie);
+
+//  Hassan Zekkouri
